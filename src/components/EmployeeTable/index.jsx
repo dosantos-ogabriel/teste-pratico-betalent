4f3f71f4e380cb17d475d9ad70497f99cdfd3051
Gabriel Santos <dosantos.ogabriel@gmail.com>
2025-02-28 20:53:48 -0300

    add responsive table cell

--- src/components/EmployeeTable/index.jsx
+++ src/components/EmployeeTable/index.jsx
@@ -1,22 +1,37 @@
+import { Fragment, useState } from "react";
 import "./employee-table-style.css";
 
+const columns = ["Foto", "Nome", "Cargo", "Data de admissão", "Telefone"];
+const columnKeys = {
+	Foto: "image",
+	Nome: "name",
+	Cargo: "job",
+	"Data de admissão": "admission_date",
+	Telefone: "phone",
+};
+
 export default function EmployeeTable({ employees }) {
+	const [selectedEmployee, setSelectedEmployee] = useState(null);
+
+	const onClickRow = (id) => {
+		const expanded = selectedEmployee === id ? null : id;
+		setSelectedEmployee(expanded);
+	};
+
 	return (
-		<>
-			<table>
-				<thead>
-					<tr>
-						<th>FOTO</th>
-						<th>NOME</th>
-						<th>CARGO</th>
-						<th>DATA DE ADMISSÃO</th>
-						<th>TELEFONE</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{employees.map((employee) => (
-						<tr key={employee.id}>
+		<table>
+			<thead>
+				<tr>
+					{columns.map((column) => (
+						<th key={column}>{column.toUpperCase()}</th>
+					))}
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{employees.map((employee) => (
+					<Fragment key={employee.id}>
+						<tr>
 							<td>
 								<img src={employee.image} alt={employee.name} />
 							</td>
@@ -24,10 +39,36 @@ export default function EmployeeTable({ employees }) {
 							<td>{employee.job}</td>
 							<td>{employee.admission_date}</td>
 							<td>{employee.phone}</td>
+							<td>
+								<button
+									onClick={() => onClickRow(employee.id)}
+									className={`${
+										selectedEmployee === employee.id ? "expanded" : ""
+									}`}
+								>
+									<img src="/src/assets/chevron-down.svg" alt="Editar" />
+								</button>
+							</td>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		</>
+						<tr
+							className={`employee-data ${
+								selectedEmployee === employee.id ? "expanded" : ""
+							}`}
+						>
+							<td colSpan={3}>
+								{columns
+									.filter((col) => !["Foto", "Nome"].includes(col))
+									.map((col) => (
+										<div className="data-row" key={col}>
+											<strong>{col}</strong>
+											<span>{employee[columnKeys[col]]}</span>
+										</div>
+									))}
+							</td>
+						</tr>
+					</Fragment>
+				))}
+			</tbody>
+		</table>
 	);
 }
